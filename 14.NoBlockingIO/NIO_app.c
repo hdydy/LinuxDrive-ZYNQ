a85a0951a9648625261b4a0fd190e0948e3c0cfd
hdydy <mk1123@sina.com>
2021-05-07 14:51:12 +0800

    添加注释并增加检测

--- 14.NoBlockingIO/NIO_app.c
+++ 14.NoBlockingIO/NIO_app.c
@@ -12,25 +12,35 @@ int main(void)
 	int fd = 0, a = 0, ret = 0;
 	struct timeval timeout;
 
+	//设置超时，上面为秒，下面为微秒
 	timeout.tv_sec = 0;
 	timeout.tv_usec = 0;
 
 	while (1)
 	{
+		//select函数会清空它所检测的socket描述符集合
+		//每次调用select()之前都必须把socket描述符重新加入到待检测的集合中
 		fd = open("/dev/nio_device", O_RDWR);
+		if (fd < 0)
+		{
+			perror("open fail!\n");
+			exit(1);
+		}
+		//初始化可读性
 		FD_ZERO(&readfds);
+		//设置可读性
 		FD_SET(fd, &readfds);
 		ret = select(fd + 1, &readfds, NULL, NULL, &timeout);
 		switch (ret)
 		{
-		case 0:
+		case 0: //超时
 			printf("Time out.\n");
 			break;
-		case 1:
+		case 1: //成功读取按键
 			read(fd, &a, sizeof(a));
 			printf("but1 value %d\n", a);
 			break;
-		default:
+		default: //其他错误
 			printf("Unknow error.\n");
 			break;
 		}
