bfa15402bcb78a4bf7f166977ab11acb818dbcbf
hdydy <mk1123@sina.com>
2021-05-10 17:21:21 +0800

    改进轮询的理解方式

--- 14.NoBlockingIO/NIO_app.c
+++ 14.NoBlockingIO/NIO_app.c
@@ -9,23 +9,22 @@
 int main(void)
 {
 	fd_set readfds;
-	int fd = 0, a = 0, ret = 0;
+	int fd = 0, a = 0, ret = 0, i = 0;
 	struct timeval timeout;
 
 	//设置超时，上面为秒，下面为微秒
-	timeout.tv_sec = 0;
+	timeout.tv_sec = 5;
 	timeout.tv_usec = 0;
 
-	while (1)
+	fd = open("/dev/nio_device", O_RDWR | O_NONBLOCK);
+	if (fd < 0)
 	{
-		//select函数会清空它所检测的socket描述符集合
-		//每次调用select()之前都必须把socket描述符重新加入到待检测的集合中
-		fd = open("/dev/nio_device", O_RDWR | O_NONBLOCK);
-		if (fd < 0)
-		{
-			perror("open fail!\n");
-			exit(1);
-		}
+		perror("open fail!\n");
+		exit(1);
+	}
+
+	while (i < 10)
+	{
 		//初始化可读性
 		FD_ZERO(&readfds);
 		//设置可读性
@@ -34,18 +33,20 @@ int main(void)
 		switch (ret)
 		{
 		case 0: //超时
-			printf("Time out.\n");
+			printf("%d.Time out.\n", i);
 			break;
 		case 1: //成功读取按键
 			read(fd, &a, sizeof(a));
-			printf("but1 value %d\n", a);
+			printf("%d.but1 value %d\n", i, a);
 			break;
 		default: //其他错误
 			printf("Unknow error.\n");
 			break;
 		}
-		close(fd);
-		sleep(1);
+
+		//sleep(1);
+		i++;
 	}
+	close(fd);
 	return 0;
 }
