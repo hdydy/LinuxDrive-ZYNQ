1fddd809727c991c84a2a8d5ae5e89d2b48b688f
hdydy <mk1123@sina.com>
2021-05-19 11:30:56 +0800

    添加注释，优化代码结构

--- 17.InputSystem/input_app.c
+++ 17.InputSystem/input_app.c
@@ -9,7 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-    int fd, ret;
+    int fd;
     struct input_event BUT_event;
 
     //打开设备
@@ -22,34 +22,26 @@ int main(int argc, char *argv[])
 
     while (1)
     {
-        ret = read(fd, &BUT_event, sizeof(struct input_event));
-        if (ret)
+        //从内核空间空间拷贝事件
+        read(fd, &BUT_event, sizeof(struct input_event));
+        switch (BUT_event.type)
         {
-            switch (BUT_event.type)
+        case EV_KEY:
+            //按键事件
+            if (BUT_event.code == KEY_0) //识别事件码
             {
-            case EV_KEY:
-                //按键事件
-                if (BUT_event.code == KEY_0) //识别事件的code
-                {
-                    if (BUT_event.value) //获得事件的传输值
-                        printf("BUT1 Press.\n");
-                    else
-                        printf("BUT1 Release.\n");
-                }
-                break;
-            case EV_SYN:
-                //同步事件
-                break;
-            default:
-                printf("Unknow message type.%d\n", BUT_event.type);
-                break;
+                if (BUT_event.value) //获得事件的传输值
+                    printf("BUT1 Press.\n");
+                else
+                    printf("BUT1 Release.\n");
             }
-        }
-        else
-        {
-            printf("Read failed!\n");
-            close(fd);
-            return 0;
+            break;
+        case EV_SYN:
+            //同步事件
+            break;
+        default:
+            printf("Unknow message type.%d\n", BUT_event.type);
+            break;
         }
     }
 
